be1cabaf194b0952d6b81568b242bdbb040d28e7
chadfayOP <chadfay@openpath.com>
2021-01-15 18:11:55 -0800

    test

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
  #!/usr/bin/env python3
 
- 
-LIST="4046 3628 4039 1876 3778 599 3290 3717 3006"
-    export IFS=" "
-    for ID in $LIST; do op core list-email-alerts $ID | egrep -A 2 '"id": 10478' | egrep -v 'emailAddresses'; done
+ list="4046,3628,4039,1876,3778,599,3290,3717,3006"
+
+for id in $list; do op core list-email-alerts $id | egrep -A 2 '"id": 10478' | egrep -v 'emailAddresses'; done
