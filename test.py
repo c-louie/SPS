054de57bf47e5560ca3d23e6da82a20bacf0bc59
chadfayOP <chadfay@openpath.com>
2021-01-15 18:16:15 -0800

    test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
  #!/usr/bin/env python3
 
- list="4046,3628,4039,1876,3778,599,3290,3717,3006"
+lst = "4046,3628,4039,1876,3778,599,3290,3717,3006"
 
-for id in $list; do op core list-email-alerts $id | egrep -A 2 '"id": 10478' | egrep -v 'emailAddresses'; done
+for id in lst:
+   do ('op core list-email-alerts $id | egrep -A 2 '"id":10478' | egrep -v 'emailAddresses''); done
