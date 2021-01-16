2234b0bd43133efca21ede7b4ce1bbf40f5c5735
chadfayOP <chadfay@openpath.com>
2021-01-15 18:27:07 -0800

    this is really just garbage

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
- #!/usr/bin/env python3
+
 
 lst = "4046,3628,4039,1876,3778,599,3290,3717,3006"
 
-for id in lst:
-   do ('op core list-email-alerts $id | egrep -A 2 '"id":10478' | egrep -v 'emailAddresses''); done
+for id_num in lst:
+    do: {"op core list-email-alerts %id_num | grep -A2 'id:10478' | egrep -v 'emailAddresses'"}
